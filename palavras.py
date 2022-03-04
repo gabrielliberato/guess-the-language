af74689734e665976904bc93aedf778a94e581ef
Gabriel Liberato <63611671+abstraktions@users.noreply.github.com>
2022-03-03 21:16:25 -0300

    aleatoriedade e primeiro funcionamento concluídos

--- palavras.py
+++ palavras.py
@@ -2,13 +2,13 @@ import pandas as pd
 from random import randint, shuffle
 
 samples = pd.read_csv('files/sample_texts.csv')
-
 languages = pd.read_csv('files/language-codes.csv')
 
 n_languages = len(languages)
 
-bloco = languages.iloc[randint(0, n_languages)]
-print(samples[samples['cod'] == bloco['cod']]['sample_text'].iloc[0])
+alternativas = ['a', 'b', 'c', 'd', 'e']
+
+bloco = languages.iloc[randint(0, n_languages-1)]
 bloco_correto = {'idioma': languages[languages['cod'] == bloco['cod']]['language'].iloc[0],
                  'texto': samples[samples['cod'] == bloco['cod']]['sample_text'].iloc[0],
                  'correto': True}
@@ -16,39 +16,29 @@ bloco_correto = {'idioma': languages[languages['cod'] == bloco['cod']]['language
 idioma_erradas = [languages[languages['cod'] == languages.iloc[randint(
     0, n_languages)]['cod']]['language'].iloc[0] for i in range(4)]
 bloco_errado = {'idioma': idioma_erradas,
+                'texto': ['-', '-', '-', '-'],
                 'correto': [False, False, False, False]}
 
+geral = {'idioma': [],
+         'texto': [],
+         'correto': []}
+for k in bloco_errado.keys():
+    geral[k] += bloco_errado[k]
+    geral[k].append(bloco_correto[k])
 
-# print(f"\nIdioma: {bloco_correto['idioma']}")
-# print(f"\nAmostra de texto: {bloco_correto['texto']}")
-# print(f"\nCorreto: {bloco_correto['correto']}\n")
+df_geral = pd.DataFrame(geral).sample(frac=1).reset_index(drop=True)
 
-print(f"\nIdioma: {bloco_errado['idioma']}")
-# print(f"\nCorreto: {bloco_errado['correto']}\n")
+correta = alternativas[df_geral[df_geral['correto'] == True].index[0]]
+print(f"\n\t\t\t\t\tAmostra do texto:\n\n{df_geral[df_geral['correto'] == True]['texto'].values[0]}\n")
 
-pos = randint(1, 5)
-err_print = bloco_errado.copy()
-print(f'{pos=}')
-for i in range(5):
-    print(i, end=": ")
-    if i == pos:
-        print(f"--> Idioma: {bloco_correto['idioma']}")
-        # print(f"Amostra de texto: {bloco_correto['texto']}")
-        # print(f"Correto: {bloco_correto['correto']}")
-    else:
-        try:
-            print(f"Idioma: {err_print['idioma'][i]}")
-            err_print['idioma'] = [k for j, k in enumerate(err_print['idioma']) if j != i]
-            err_print['correto'] = [k for j, k in enumerate(err_print['idioma']) if j != i]
-            # print(f"Correto: {bloco_errado['correto'][i]}")
-        except:
-            try:
-                print(f"Idioma: {bloco_errado['idioma'][i+1]}")
-                err_print['idioma'] = [k for j, k in enumerate(err_print['idioma']) if j != i]
-                err_print['correto'] = [k for j, k in enumerate(err_print['idioma']) if j != i]
-            # print(f"\nCorreto: {bloco_errado['correto'][i-1]}")
-            except:
-                print(f"Idioma: {bloco_errado['idioma'][i-1]}")
-                err_print['idioma'] = [k for j, k in enumerate(err_print['idioma']) if j != i]
-                err_print['correto'] = [k for j, k in enumerate(err_print['idioma']) if j != i]
-            
+for alt, i in enumerate(df_geral['idioma']):
+    print(f">> ({alternativas[alt]}) {i}")
+
+resposta = input('\n\t\t\t\t\tO trecho acima pertence a qual idioma? ').lower()
+
+if resposta == correta:
+    print("\n\t\t\t\t\tVocê acertou!\n")
+else:
+    print("\n\t\t\t\t\tVocê errou ):")
+    print(f"\n\t\t\t\t\tA alternativa correta era: {df_geral[df_geral['correto'] == True]['idioma'].values[0]}\n")
+
